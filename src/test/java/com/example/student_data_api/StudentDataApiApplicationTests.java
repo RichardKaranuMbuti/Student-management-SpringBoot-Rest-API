70d66b637ddff6512b385222b98a624fd61d1360
RichardKaranuMbuti <Officialforrichardk@gmail.com>
2024-06-13 14:13:53 +0300

    initial project configuration

---
+++ src/test/java/com/example/student_data_api/StudentDataApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.student_data_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentDataApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
